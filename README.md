d511a6ef5b42307e61e4a1f45fe914dd13896557
Friedrich Wiemer <friedrich.wiemer@rub.de>
2014-03-24 22:50:17 +0100

    changed trivium state to one shiftregister updated generator function to include init-step rewrote updateState() as trivium()

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
-trivium
+Trivium
 =======
 
 different implementations of the trivium stream-cipher
+
+
+Planning
+=======
+
+Implementations in Python (for debugging) in C and VHDL
+and Haskell (for the fun)
--- src/python/trivium.py
+++ src/python/trivium.py
@@ -1,75 +1,85 @@
-import collections
-import binascii
-import itertools
+from collections import deque
+from itertools import repeat
 
 
-reg_1 = None
-reg_2 = None
-reg_3 = None
+register = None
+counter = 0
+
 
 def output():
     pass
 
 
-def update_state():
-    global reg_1
-    global reg_2
-    global reg_3
+def trivium(iv, key):
+    global register
+    global counter
 
-    t_1 = reg_1[65] ^ reg_1[92]
-    t_2 = reg_2[67] ^ reg_2[82]
-    t_3 = reg_3[64] ^ reg_3[109]
-    
-    out = t_1 ^ t_2 ^ t_3
+    use_key = "{0:080b}".format(key)  # print key in binary
+    if len(use_key) > 80:  # reverse string and cut off 80b
+        use_key = use_key[:(len(use_key)-81):-1]
+    else:
+        use_key = use_key[::-1]
 
-    t_1 = t_1 ^ (reg_1[90] & reg_1[91]) ^ reg_2[76]
-    t_2 = t_2 ^ (reg_2[80] & reg_2[81]) ^ reg_3[85]
-    t_3 = t_3 ^ (reg_3[107] & reg_3[108]) ^ reg_1[68]
+    use_iv = "{0:080b}".format(iv)  # print iv in binary
+    if len(use_iv) > 80:  # reverse string and cut off 80b
+        use_iv = use_iv[:(len(use_iv)-81):-1]
+    else:
+        use_iv = use_iv[::-1]
 
-    # Shift by one to MSB
-    reg_1.rotate(1)
-    reg_2.rotate(1)
-    reg_3.rotate(1)
+    # len 93
+    init_list = list(map(int, list(use_key)))
+    init_list += list(repeat(0, 13))
+    init_list += list(map(int, list(use_iv)))
+    init_list += list(repeat(0, 4))
+    init_list += list(repeat(0, 108))
+    init_list += list([1, 1, 1])
+    register = deque(init_list)
 
-    reg_1[0] = t_3
-    reg_2[0] = t_1
-    reg_3[0] = t_2
-
-    return out
-
-
-def init():
-    global reg_1
-    global reg_2
-    global reg_3
-
+    # init state
     for i in range(4*288):
-        update_state()
+        t_1 = register[65] ^ register[92]
+        t_2 = register[161] ^ register[176]
+        t_3 = register[242] ^ register[287]
+
+        t_1 = t_1 ^ (register[90] & register[91]) ^ register[170]
+        t_2 = t_2 ^ (register[174] & register[175]) ^ register[263]
+        t_3 = t_3 ^ (register[285] & register[287]) ^ register[68]
+
+        register.rotate(1)
+
+        register[0] = t_3
+        register[93] = t_1
+        register[177] = t_2
+
+    # output keystream
+    while counter < 2**64:
+        t_1 = register[65] ^ register[92]
+        t_2 = register[161] ^ register[176]
+        t_3 = register[242] ^ register[287]
+
+        out = t_1 ^ t_2 ^ t_3
+
+        t_1 = t_1 ^ (register[90] & register[91]) ^ register[170]
+        t_2 = t_2 ^ (register[174] & register[175]) ^ register[263]
+        t_3 = t_3 ^ (register[285] & register[287]) ^ register[68]
+
+        register.rotate(1)
+
+        register[0] = t_3
+        register[93] = t_1
+        register[177] = t_2
+
+        yield out
 
 
 def main():
-    global reg_1
-    global reg_2
-    global reg_3
 
     KEY = 0xFFFFFFFFFFFFFFFFFFF0
-    use_key = bin(KEY)[2:82] # Cut off for 80 Bit
-    use_key = use_key[::-1]  # Invert string
     IV = 0xFFFFFFFFFFFFFFFFFFFF
-    use_iv = bin(IV)[2:82]
-    use_iv = use_iv[::-1]
-    # Transform key to binary list and append 13 Zeros
-    reg_1 = collections.deque(map(int,list(use_key)) + list(itertools.repeat(0, 13))) #len 93
-    reg_2 = collections.deque(map(int,list(use_iv)) + list(itertools.repeat(0, 4)))  # len 84
-    reg_3 = collections.deque(list(itertools.repeat(0, 108)) + list([1,1,1]))  # len 111
-    init()
+    keystream = trivium(IV, KEY)
 
-    print reg_3
-    print reg_2
-    # Use /dev/urandom later
-
-    for i in range(15):
-        print update_state()
+    for i in range(8*4):
+        print(keystream.__next__())
 
 if __name__ == "__main__":
     main()
