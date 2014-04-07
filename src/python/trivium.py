0baf061eaf717a5f5dc7d6c2e3edd9500804078e
Friedrich Wiemer <friedrich.wiemer@rub.de>
2014-04-07 15:58:49 +0200

    changed internal state to 3 shiftregs use of hex_to_bit and vv functions for endianess

--- src/python/trivium.py
+++ src/python/trivium.py
@@ -7,20 +7,29 @@ class Trivium:
     def __init__(self, key, iv):
         """in the beginning we need to transform the key as well as the IV.
         Afterwards we initialize the state."""
-        self.state = None
+        self.state0 = None
+        self.state1 = None
+        self.state2 = None
         self.counter = 0
-        self.key = self._setLength(key)
-        self.iv = self._setLength(iv)
+        self.key = key  # self._setLength(key)
+        self.iv = iv  # self._setLength(iv)
 
         # Initialize state
         init_list = list(map(int, list(self.key)))
-        init_list += list(repeat(0, 13))
-        init_list += list(map(int, list(self.iv)))
-        init_list += list(repeat(0, 4))
-        init_list += list(repeat(0, 108))
-        init_list += list([1, 1, 1])
-        self.state = deque(init_list)
+        init_list += list(repeat(0, 20))
+        self.state0 = deque(init_list)
 
+        init_list = list(map(int, list(self.iv)))
+        init_list += list(repeat(0, 4))
+        self.state1 = deque(init_list)
+
+        init_list = list(repeat(0, 108))
+        init_list += list([1, 1, 1])
+        self.state2 = deque(init_list)
+
+        print(self.state0, len(self.state0))
+        print(self.state1, len(self.state1))
+        print(self.state2, len(self.state2))
         # Do 4 full cycles, drop output
         for i in range(4*288):
             self._gen_keystream()
@@ -51,29 +60,35 @@ class Trivium:
 
     def _gen_keystream(self):
         """this method generates triviums keystream"""
-        t_1 = self.state[65] ^ self.state[92]
-        t_2 = self.state[161] ^ self.state[176]
-        t_3 = self.state[242] ^ self.state[287]
+
+        a_1 = self.state0[90] & self.state0[91]
+        a_2 = self.state1[81] & self.state1[82]
+        a_3 = self.state2[108] & self.state2[109]
+
+        t_1 = self.state0[65] ^ self.state0[92]
+        t_2 = self.state1[68] ^ self.state1[83]
+        t_3 = self.state2[65] ^ self.state2[110]
 
         out = t_1 ^ t_2 ^ t_3
 
-        t_1 = t_1 ^ (self.state[90] & self.state[91]) ^ self.state[170]
-        t_2 = t_2 ^ (self.state[174] & self.state[175]) ^ self.state[263]
-        t_3 = t_3 ^ (self.state[285] & self.state[286]) ^ self.state[68]
+        s_1 = a_3 ^ self.state0[68] ^ t_3
+        s_2 = a_1 ^ self.state1[77] ^ t_1
+        s_3 = a_2 ^ self.state2[86] ^ t_2
 
-        self.state.rotate(1)
+        self.state0.rotate(1)
+        self.state1.rotate(1)
+        self.state2.rotate(1)
 
-        self.state[0] = t_3
-        self.state[93] = t_1
-        self.state[177] = t_2
+        self.state0[0] = s_1
+        self.state1[0] = s_2
+        self.state2[0] = s_3
 
         return out
 
 
 def main():
-
-    KEY = 0x80000000000000000000
-    IV = 0x00000000000000000000
+    KEY = hex_to_bits("0F62B5085BAE0154A7FA")[::-1]
+    IV = hex_to_bits("288FF65DC42B92F960C7")[::-1]
     trivium = Trivium(KEY, IV)
 
     # Check python version
@@ -85,14 +100,28 @@ def main():
         print("invalid python version")
         return
 
-    keystream = ""
-    for i in range(16):
-        key_byte = 0
-        for j in range(8):
-            key_bit = next_key_bit()
-            key_byte = (key_byte << 1) | key_bit
-        keystream += "{0:02X}".format(key_byte)
-    print(keystream)
+    for i in range(1):
+        keystream = []
+        for j in range(128):
+            keystream.append(next_key_bit())
+        print(bits_to_hex(keystream))
+
+# Convert strings of hex to strings of bytes and back, little-endian style
+_allbytes = dict([("%02X" % i, i) for i in range(256)])
+
+
+def _hex_to_bytes(s):
+    return [_allbytes[s[i:i+2].upper()] for i in range(0, len(s), 2)]
+
+
+def hex_to_bits(s):
+    return [(b >> i) & 1 for b in _hex_to_bytes(s)
+            for i in range(8)]
+
+
+def bits_to_hex(b):
+    return "".join(["%02X" % sum([b[i + j] << j for j in range(8)])
+                    for i in range(0, len(b), 8)])
 
 
 if __name__ == "__main__":
